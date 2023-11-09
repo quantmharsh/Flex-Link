47a6d61920287ef587431e9e5482023258e5664f
Harsh Srivastava <srivastavah240@gmail.com>
2023-11-09 11:34:47 +0530

    File & Folder structure completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Box } from '@mui/material';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import  Home  from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">                 
+    {/* xl means extra large screens sizes  //m is for margin  */}
+      <Box width="400px" sx={{width:{xl:'1490px'}}} m="auto">
+        <Navbar/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </BrowserRouter>
+    <Footer/>
+      </Box>
     </div>
   );
 }
